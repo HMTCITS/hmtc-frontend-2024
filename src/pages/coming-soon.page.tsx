9653fc9e8c6a5597a3da3df0278aaa5c3e67e15b
faralha <faralhaonly@gmail.com>
2024-12-20 05:10:44 +0700

    feat: coming-soon half done

---
+++ src/pages/coming-soon.page.tsx
@@ -0,0 +1,31 @@
+import { HiOutlineArrowNarrowLeft } from 'react-icons/hi';
+
+import BaseLink from '@/components/links/BaseLink';
+import Typography from '@/components/typography/Typography';
+
+export default function comingSoon() {
+  return (
+    <main>
+      <section className='relative h-screen overflow-clip bg-[#201F1F] text-white'>
+        <div className='flex h-full w-full -translate-y-6 flex-col items-center justify-center gap-y-1'>
+          <h1 className='bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text font-libre-caslon-condensed text-[10rem] leading-tight text-transparent sm:text-[6rem] md:text-[9rem]'>
+            Coming Soon
+          </h1>
+          <Typography variant='s1' className='mb-0 text-center font-adelphe'>
+            This page is still under development. Stay Tuned!
+          </Typography>
+
+          <div className='flex items-center gap-x-1'>
+            <BaseLink
+              href='/'
+              className='flex items-center gap-x-1 font-primary text-lg underline decoration-white underline-offset-2 transition-colors duration-150 hover:decoration-gray-600'
+            >
+              <HiOutlineArrowNarrowLeft size={22} />
+              Go Back
+            </BaseLink>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
+}
