900b8975a51b21689dc5160285f810fb8214158f
faralha <faralhaonly@gmail.com>
2024-12-20 05:04:17 +0700

    feat: 500 page final (without routing) + 400 page fix

--- src/pages/500.page.tsx
+++ src/pages/500.page.tsx
@@ -3,17 +3,17 @@ import { HiOutlineArrowNarrowLeft } from 'react-icons/hi';
 import BaseLink from '@/components/links/BaseLink';
 import Typography from '@/components/typography/Typography';
 
-export default function NotFoundPage() {
+export default function serverError() {
   return (
     <main>
-      <section className='relative h-screen overflow-clip bg-[#201F1F] text-white'>
+      <section className='relative h-screen overflow-clip'>
         <div className='flex h-full w-full -translate-y-6 flex-col items-center justify-center gap-y-1'>
           <Typography variant='s3'>ERROR</Typography>
           <h1 className='bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text font-libre-caslon-condensed text-[10rem] leading-none text-transparent sm:text-[6rem] md:text-[9rem]'>
-            404
+            500
           </h1>
           <Typography variant='s1' className='mb-0 text-center font-adelphe'>
-            The page you&#39;re looking for does not exist.
+            Internal Server Error. Please try again later.
           </Typography>
 
           <div className='flex items-center gap-x-1'>
