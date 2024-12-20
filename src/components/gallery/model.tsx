d5b345f1bc4ce152100afe10005908607a5a9267
faralha <faralhaonly@gmail.com>
2024-12-20 20:49:01 +0700

    feat: gallery model

---
+++ src/components/gallery/model.tsx
@@ -0,0 +1,29 @@
+import Typography from '@/components/typography/Typography';
+
+export default function Model() {
+  return (
+    <section className='flex flex-col items-center gap-2 bg-white p-10'>
+      <Typography
+        variant='s2'
+        className='bg-gradient-to-tr from-blue-600 to-blue-400 bg-clip-text text-transparent'
+      >
+        ACCESS RESTRICTED
+      </Typography>
+      <Typography variant='h1' className='font-adelphe'>
+        Please enter your NRP to access this page.
+      </Typography>
+      <form action='' className='flex w-full flex-col justify-center'>
+        <input
+          type='text'
+          name=''
+          id=''
+          placeholder='NRP'
+          className='w-full rounded-sm px-2 py-1 outline outline-offset-2'
+        />
+        <button type='submit' className='w-100 mt-5 text-center underline'>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
--- src/pages/gallery.page.tsx
+++ src/pages/gallery.page.tsx
@@ -1,9 +1,17 @@
 import GalleryCard from '@/components/gallery/card';
+import Model from '@/components/gallery/model';
 import Typography from '@/components/typography/Typography';
 
 export default function gallery() {
   return (
     <main className='my-28 flex min-h-screen flex-col items-center justify-center'>
+      {/* Model Login Screen */}
+      <section className='fixed inset-0 z-[100] bg-black bg-opacity-50' />
+      <section className='fixed inset-0 z-[100] flex items-center justify-center'>
+        <Model />
+      </section>
+
+      {/* Gallery */}
       <section className='mb-10 flex flex-col items-center justify-center gap-y-2'>
         <Typography
           variant='s2'
@@ -11,13 +19,16 @@ export default function gallery() {
         >
           GALLERY HMTC
         </Typography>
-        <Typography variant='j2' className='font-adelphe'>
+        <Typography
+          variant='j2'
+          className='px-2 text-center font-adelphe sm:px-5 md:px-3'
+        >
           From Capturing Moments to Sharing Memories
         </Typography>
       </section>
 
-      <section className='mt-5 grid grid-cols-4 grid-rows-3 gap-20'>
-        {/* todo: dynamic import, data fetch, etc */}
+      <section className='mt-5 grid grid-cols-1 grid-rows-1 gap-20 sm:grid-rows-1 sm:gap-5 md:grid-cols-3 md:gap-10 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4'>
+        {/* todo: fetch statis dari src/images? */}
         <GalleryCard />
         <GalleryCard />
         <GalleryCard />
